58ee17a5122d39d9205c8bc358eff03a75e97e98
Yefry Sanchez <yefrymsp@gmail.com>
2024-10-09 15:00:18 -0400

    UPDATE cors

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,49 @@
-import express, { Request, Response } from "express"
-import { config } from "dotenv"
-import authRounter from "./routes/authRouter"
-import employeeRouter from "./routes/employeeRouter"
-import categoryRounter from "./routes/categoryRouter"
-import prisma from "./db/prismaClient"
-import productsRouter from "./routes/productsRouter"
-import cors from "cors"
+import express, { Request, Response } from "express";
+import { config } from "dotenv";
+import authRounter from "./routes/authRouter";
+import employeeRouter from "./routes/employeeRouter";
+import categoryRounter from "./routes/categoryRouter";
+import prisma from "./db/prismaClient";
+import productsRouter from "./routes/productsRouter";
+import cors from "cors";
 
-config()
-const app = express()
+config();
+const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 //cors
-
-app.use(cors({ origin: 'http://localhost:5173' }));
-
+const corsOptions = {
+  origin: ["https://ezpos.pages.dev", "http://localhost:5500"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // If you need to include credentials in requests
+  optionsSuccessStatus: 204, // For legacy browser support
+};
+app.use(cors(corsOptions));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.get("/", (req:Request, res:Response) => {
-    res.send("Hello World")
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
 
 //Login Register
-app.use("/api/auth", authRounter)
-// Employees 
-app.use("/api/employees", employeeRouter)
-//Product Category 
-app.use("/api/productcategory", categoryRounter)
+app.use("/api/auth", authRounter);
+// Employees
+app.use("/api/employees", employeeRouter);
+//Product Category
+app.use("/api/productcategory", categoryRounter);
 //Products
-app.use("/api/products", productsRouter)
+app.use("/api/products", productsRouter);
 
 // Handle SIGINT to clean up Prisma client
-process.on('SIGINT', async () => {
-    console.log('Shutting down gracefully...');
-    await prisma.$disconnect();
-    process.exit(0);
+process.on("SIGINT", async () => {
+  console.log("Shutting down gracefully...");
+  await prisma.$disconnect();
+  process.exit(0);
 });
 
 app.listen(PORT, () => {
-    console.log(`Running on port: ${PORT}`)
-})
+  console.log(`Running on port: ${PORT}`);
+});
