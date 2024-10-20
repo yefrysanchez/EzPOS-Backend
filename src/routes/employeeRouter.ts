8ea93728ed67a7b27defd15cdfbb6b4741b6bd9b
Yefry Sanchez <yefrymsp@gmail.com>
2024-10-20 14:39:33 -0400

    editEmployees Route added

--- src/routes/employeeRouter.ts
+++ src/routes/employeeRouter.ts
@@ -1,10 +1,11 @@
 import {Router} from "express"
-import { createEmployee, deleteEmployee, getAllEmployees, getEmployee } from "../controllers/employeeController"
+import { createEmployee, deleteEmployee, EditEmployee, getAllEmployees, getEmployee } from "../controllers/employeeController"
 const employeeRouter = Router()
 
 employeeRouter.post("/", createEmployee)
 employeeRouter.get("/", getAllEmployees)
 employeeRouter.get("/:id", getEmployee)
+employeeRouter.patch("/", EditEmployee)
 employeeRouter.delete("/:id", deleteEmployee)
 
 
